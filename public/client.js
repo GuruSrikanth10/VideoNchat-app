72a7d31043b639a97db947c24aeb7b5d293e8be3
GuruSrikanth10 <121211627+GuruSrikanth10@users.noreply.github.com>
2024-07-12 13:45:03 +0530

    Updated host

--- public/client.js
+++ public/client.js
@@ -35,7 +35,7 @@ For testing locally we can run a server on some port by using this command
 */
   const peer = new Peer(undefined, {
    
-  host: "13.201.168.193",
+  host: process.env.HOST,
     // host: "https://peerjs.com/peerserver",
     // port: 9000 ,
     // path: "/myapp",
